efadc73ed030597eeb25429b73de5e8e37424890
Greg Piotraschke <gregp@xpinova.com>
2017-11-22 17:44:45 -0600

    Add Syncfusion project

---
+++ NewProject.Old/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewProject.Web.Startup))]
+namespace NewProject.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- NewProject.Tests/Controllers/HomeControllerTest.cs
+++ NewProject.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NewProject.Web;
 using NewProject.Web.Controllers;
 
 namespace NewProject.Tests.Controllers
